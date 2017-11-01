61ba444d376e5bed2cfd03595ca92fc5bd847a8f
Omar <omarlm1992@gmail.com>
2017-11-01 12:00:13 +0000

    Estructura html mejorada y css

--- tema3/prueba-listado-PDO.php
+++ tema3/prueba-listado-PDO.php
@@ -26,37 +26,38 @@ $consultation->execute();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Prueba_T3</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
     <link href="dwes.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div id="encabezado">
-        <?php if(!isset($_POST['SLCTfamilia'])): //Si la familia no está definida y es null, las mostramos
-            $_SESSION['SLCTfamilia'] = null; ?>
-            <h1>Tarea: Listado de productos de una familia</h1>
-            <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-                <label>Familia: </label>
-                <select name='SLCTfamilia'>;
-                    <?php while($row = $consultation->fetch()):?>
-                            <option value="<?=$row['cod']?>"><?=$row['nombre']?></option>
-                    <?php endwhile; ?>
-                </select>
-                <input type="submit" value='Mostrar Productos' />
-            </form>
+        <h1>Tarea: Listado de productos de una familia</h1>
     </div>
     <div id="contenido">
-        <?php elseif(isset($_POST['SLCTfamilia'])):  //Sino si la familia está definida, mostramos sus productos
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        <label>Familia: </label>
+        <select name='SLCTfamilia'>;
+            <?php while($row = $consultation->fetch()):?>
+                    <option value="<?=$row['cod']?>"><?=$row['nombre']?></option>
+            <?php endwhile; ?>
+        </select>
+        <input type="submit" value='Mostrar Productos' />
+    </form>
+        <?php if(isset($_POST['SLCTfamilia'])):  //Sino si la familia está definida, mostramos sus productos
             include('prueba-dbconex-PDO.php');
             $family = $_POST['SLCTfamilia'];
             $consultation = $dbh->prepare("SELECT nombre_corto, PVP FROM producto WHERE familia = ? ORDER BY nombre_corto ASC");
             $consultation->setFetchMode(PDO::FETCH_ASSOC);
                 if($consultation->execute(array($_POST['SLCTfamilia']))):?>
-                    <h2>Productos de la familia:<?= $family?></h2>
+                    <h2>Productos de la familia: <?= $family?></h2>
+                    <hr>
                     <?php while($row = $consultation->fetch()):?>
-                            <p><?=$row['nombre_corto']?>:<?=$row['PVP']?><button type="button" name="btn-edit">Editar</button></p>
+                            <p><span><?=$row['nombre_corto']?></span>: <?=$row['PVP']?>€<button type="button" name="btn-edit">Editar</button></p>
                     <?php endwhile?>
                 <?php endif; ?>
-       <?php endif;?>
+        <?php endif;?>
     </div>
-    <div id="pie"></div>
+    <div id="pie">
+    </div>
 </body>
 </html>
