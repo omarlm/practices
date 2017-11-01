707ac7607990c5c1e982b1d642933814e962633c
Yrm86 <yrm86@hotmail.es>
2017-11-01 10:40:57 +0000

    reestructurado el html

--- tema3/prueba-listado-PDO.php
+++ tema3/prueba-listado-PDO.php
@@ -17,8 +17,6 @@ $consultation->execute();
 //$consultation->closeCursor();
 //Incluimos el fichero de desconexión a la base de datos
 //include('DBdesconex.php');
-
-
 ?>
 
 <!DOCTYPE html>
@@ -31,7 +29,6 @@ $consultation->execute();
     <link href="dwes.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div class="contenido">
     <div id="encabezado">
         <?php if(!isset($_POST['SLCTfamilia'])): //Si la familia no está definida y es null, las mostramos
             $_SESSION['SLCTfamilia'] = null; ?>
@@ -41,12 +38,12 @@ $consultation->execute();
                 <select name='SLCTfamilia'>;
                     <?php while($row = $consultation->fetch()):?>
                             <option value="<?=$row['cod']?>"><?=$row['nombre']?></option>
-                            <!--Necesito una variable que me guarde el cod de la familia porque es lo que tengo que relacionar-->
                     <?php endwhile; ?>
                 </select>
                 <input type="submit" value='Mostrar Productos' />
             </form>
     </div>
+    <div id="contenido">
         <?php elseif(isset($_POST['SLCTfamilia'])):  //Sino si la familia está definida, mostramos sus productos
             include('prueba-dbconex-PDO.php');
             $family = $_POST['SLCTfamilia'];
@@ -55,12 +52,11 @@ $consultation->execute();
                 if($consultation->execute(array($_POST['SLCTfamilia']))):?>
                     <h2>Productos de la familia:<?= $family?></h2>
                     <?php while($row = $consultation->fetch()):?>
-                            <p><?=$row['nombre_corto']?>:<?=$row['PVP']?><button type="">Editar</button></p>
+                            <p><?=$row['nombre_corto']?>:<?=$row['PVP']?><button type="button" name="btn-edit">Editar</button></p>
                     <?php endwhile?>
                 <?php endif; ?>
        <?php endif;?>
-
+    </div>
     <div id="pie"></div>
-</div>
 </body>
 </html>
